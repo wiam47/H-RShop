133c4f41d4ad375d8465af4945532b09a00417ca
wiam47 <wiam67bissi@gmail.com>
2021-09-01 18:13:04 +0100

    upload

---
+++ index.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Montres</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<div class="header">
+    <div class="logo">
+        <a href="index.php"><img id="logo" src="photos/logo2.png"></a>
+    </div>
+    <div class="icons"> 
+        <div class="icon searchBox">
+
+            <input class="searchInput" type="text" name="" placeholder="Search..">
+            <button type="submit" class="searchButton" href="#">
+                <i class="material-icons fa fa-search">    
+                </i>
+            </button>
+        </div>
+        <div class="icon">
+            <a href="#"><i class="fa fa-user"></i></a>
+        </div>
+        <div class="icon">
+            <a href="#"><i class="fa fa-shopping-basket"></i></a>
+        </div>
+        <div class="icon">
+            <a href="#"><i class="fa fa-globe"></i></a>
+        </div>
+    </div>
+</div>  
+<div class="navbar">
+  <a href="#">HOME</a> 
+  <a href="#">WATCH</a> 
+  <a href="#">ACCESSORIES</a> 
+  <a href="#">OFFER</a> 
+  <a href="#">SUMMER OFFER</a>
+</div>
+
+<div class="slideshow-container">
+
+<div class="mySlides fade">
+  <img src="photos/slide1.jpg" style="width:100%">
+</div>
+
+<div class="mySlides fade">
+  <img src="photos/slide2.jpg" style="width:100%">
+</div>
+
+<div class="mySlides fade">
+  <img src="photos/slide3.jpg" style="width:100%">
+</div>
+<div class="dots">
+  <span class="dot"></span> 
+  <span class="dot"></span> 
+  <span class="dot"></span> 
+</div>
+</div>
+ <div class="grid">
+ <div>
+   <img src="photos/montre1.jpg" alt="">
+ </div>
+ <div>
+   <img src="photos/montre2.jpg" alt="">
+ </div>
+ <div>
+   <img src="photos/montre3.jpg" alt="">
+ </div>
+ <div>
+   <img src="photos/montre1.jpg" alt="">
+ </div>
+ </div>
+<script src="dyn.js"></script>
+
+</body>
+</html>

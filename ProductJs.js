d2a245675b2820a1df52e2e196b4dd1430c812fe
ImadAkhraz <56758045+ImadAkhraz@users.noreply.github.com>
2021-11-28 23:23:28 +0100

    changed pdct-info

--- ProductJs.js
+++ ProductJs.js
@@ -5,10 +5,39 @@ function buy() {
 }
   
 function pdctQuantity(method) {
-  var NbrProduct = document.getElementById('quantity');
-  if (method=="add") {
-    NbrProduct.innerHTML = Number(NbrProduct.textContent)+1;
-  } else if (method=="minus" && Number(NbrProduct.textContent) != 1) {
-    NbrProduct.innerHTML = Number(NbrProduct.textContent)-1;
+  var nbrProduct = document.getElementById('quantity');
+  var stockLimitValue = Number(document.getElementById('stock_limit').textContent);
+  var nbrProductValue = Number(nbrProduct.textContent);
+  if (method=="add" && nbrProductValue != 9) {
+    nbrProduct.innerHTML = nbrProductValue+1;
+  } else if (method=="minus" && nbrProductValue != 1) {
+    nbrProduct.innerHTML = nbrProductValue-1;
   }
 }
+
+
+$(document).ready(function() {
+  $(".color .options .option:first-child").addClass("selected-color-option");
+  $(".color .options .option").click(function() {
+    $(".selected-color-option").addClass("option");
+    $(".selected-color-option").removeClass("selected-color-option");
+    $(this).removeClass("option");
+    $(this).addClass("selected-color-option");
+  });
+  
+  $(".top-size .options .option:first-child").addClass("selected-size-option");
+  $(".top-size .option").click(function() {
+    $(".top-size .selected-size-option").addClass("option");
+    $(".top-size .selected-size-option").removeClass("selected-size-option");
+    $(this).removeClass("option");
+    $(this).addClass("selected-size-option");
+  });
+
+  $(".lowerbody-size .options .option:first-child").addClass("selected-size-option");
+  $(".lowerbody-size .option").click(function() {
+    $(".lowerbody-size .selected-size-option").addClass("option");
+    $(".lowerbody-size .selected-size-option").removeClass("selected-size-option");
+    $(this).removeClass("option");
+    $(this).addClass("selected-size-option");
+  });
+});
